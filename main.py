289dd582daa929eecd9cb828e755d9cd539f6041
JoelBradley <joelbradley14@gmail.com>
2014-10-03 12:48:49 +0100

    Added window creation

--- main.py
+++ main.py
@@ -6,6 +6,20 @@
   Adam Cook
   Ben Domokos
 """
+import pygame
 
 #so it begins
 print "Hello, world!"
+
+class Game():
+
+    def __init__(self, width, height):
+        self.window = pygame.display.set_mode((width,height))
+        
+
+if __name__ == "__main__":
+    width = 600
+    height = 400
+    game = Game(width, height)
+
+pygame.quit()
