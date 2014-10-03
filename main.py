c8b10bc52ce4c145277bbb5c84e0401eea673f3d
GJClaridge <claridgg@uni.coventry.ac.uk>
2014-10-03 13:20:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,15 @@ class Game():
 
     def __init__(self, width, height):
         self.window = pygame.display.set_mode((width,height))
+        fpsClock = pygame.time.Clock()
+        font = pygame.font.Font('freesansbold.ttf', 36)
+        run()
         
+    def run():
+      while True:
+          pygame.display.update()
+          fpsClock.tick(60)
+          
 
 if __name__ == "__main__":
     width = 600
