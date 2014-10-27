7b49aabd497416c7423ea765abf0eab5a203283c
JoelBradley <joelbradley14@gmail.com>
2014-10-27 12:45:40 +0000

    Added tile speed

--- Entity.py
+++ Entity.py
@@ -6,9 +6,12 @@ class Entity(object):
 		self.level=level
 		self.x=x
 		self.y=y
+		self.centreX = x
+		self.centreY = y
+		self.ticks = 0
 
 	def tick(self):
-		pass
+                self.ticks+=1
 		
 	def move(self,xa,ya):
 		if xa != 0 and ya != 0:
@@ -28,9 +31,9 @@ class Entity(object):
 			if xa > 0:
 				self.movingDir = 3
 			
-			
-			self.x += xa
-			self.y += ya
+			if self.ticks%self.getTileUnder().getSpeed() ==0:
+                                self.x += xa
+                                self.y += ya
 			
 			
 			return False
@@ -40,14 +43,13 @@ class Entity(object):
 	
 	
 	def getTileUnder(self):
-		return self.level.getTile(self.x>>5, self.y>>5)
+		return self.level.getTile(self.centreX>>5, self.centreY>>5)
 	
 
 	def hasCollided(self,xa, ya):
 		return False
 
 	def isSolidTile(self,xa, ya, x, y):
-
 		lastTile = self.level.getTile((self.x + x) >>5, (self.y + y) >>5)
 		nextTile = self.level.getTile((self.x + x + xa) >>5,(self.y + y + ya) >> 5)
 		if lastTile != nextTile and nextTile.isSolid:
