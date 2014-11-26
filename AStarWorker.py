6ddab06336f027738c2cad3fbaff37d695f8352b
JoelBradley <joelbradley14@gmail.com>
2014-11-26 12:04:23 +0000

    Fixed the same mistakes here too!

--- AStarWorker.py
+++ AStarWorker.py
@@ -14,7 +14,7 @@ def getTile(x,y):
 
 def getDistance(a,b):
     dx = a[0] - b[0]
-    dy = a[1] - b[0]
+    dy = a[1] - b[1]
     return math.sqrt(dx*dx+dy*dy)
 
 def inList(l,i):
@@ -26,55 +26,64 @@ def inList(l,i):
 
 
 
-def findPath(start,goal):
-    startTime = time.time()
-    openList = []
-    closedList = []
-    currentNode = Node(start,None,0,getDistance(start,goal))
-    openList.append(currentNode)
-    while len(openList) >0:
-        sorted(openList,key=lambda i: i.totalCost)
-        currentNode = openList[0] #only use node with lowest cost
-        if currentNode.pos==goal:
-            path = []
-            while currentNode.parent != None:#goes until reaches the start
-                path.append(currentNode)
-                currentNode = currentNode.parent
-            openList = []
-            closedList = []
-            print "Time taken:", time.time()-startTime
-            return path
-        openList.remove(currentNode)
-        closedList.append(currentNode)
-        for i in range(9):
-            if i==0:
-                continue#ignore current tile
-            x = currentNode.pos[0]
-            y  = currentNode.pos[1]
-            dx = (i % 3) -1
-            dy = (i / 3) -1
-            tile = getTile(x+dx,y+dy)
-            if tile == None or tile == Tile.void:
-                continue
-            if tile.isSolid:
-                #print 'solid'
-                continue
-            tilePos = (x+dx,y+dy)
-            costSoFar = currentNode.costSoFar + getDistance(currentNode.pos,tilePos)
-            costSoFar*=tile.getSpeed()
-            distanceToEnd = getDistance(tilePos,goal)
-            node = Node(tilePos,currentNode,costSoFar,distanceToEnd)
-            if inList(closedList,tilePos) and costSoFar >= node.costSoFar: #checks if node has already been used,or if you are going backwards
-                continue
-            if not inList(openList,tilePos) or costSoFar < node.costSoFar:#only add if it's not alredy there
-                openList.append(node)
+def lookForFastest(self,l):
+                currentSmallest =0
+                bestNode = None
+                if len(l)==1:
+                        return l[0]
+                for i in l:
+                        if i.costSoFar<currentSmallest or currentSmallest==0:
+                                currentSmallest =i.costSoFar
+                                bestNode = i
+                return bestNode
+
+
+def findPath(self,start,goal):
+	openList = []
+	closedList = []
+	currentNode = Node(start,None,0,self.getDistance(start,goal))
+	openList.append(currentNode)
+	while len(openList) >0:
+
+		#sorted(openList,key=lambda i: i.totalCost)
+		currentNode = self.lookForFastest(openList) #only use node with lowest cost
+		if currentNode.pos==goal:
+			path = []
+			while currentNode.parent != None:#goes until reaches the start
+				path.append(currentNode)
+				currentNode = currentNode.parent
+			openList = []
+			closedList = []
+			return path
+		openList.remove(currentNode)
+		closedList.append(currentNode)
+		for i in range(9):
+			if i==4:
+				continue#ignore current tile
+			x = currentNode.pos[0]
+			y  = currentNode.pos[1]
+			dx = (i % 3) -1
+			dy = (i / 3) -1
+			tile = self.getTile(x+dx,y+dy)
+			if tile == None or tile == Tile.void:
+				continue
+			if tile.isSolid:
+				#print 'solid'
+				continue
+			tilePos = (x+dx,y+dy)
+			costSoFar = currentNode.costSoFar + (self.getDistance(currentNode.pos,tilePos)+tile.getSpeed())
+			distanceToEnd = self.getDistance(tilePos,goal)
+			node = Node(tilePos,currentNode,costSoFar,distanceToEnd)
+			if self.inList(closedList,tilePos) and costSoFar >= node.costSoFar: #checks if node has already been used,or if you are going backwards
+				continue
+			if not self.inList(openList,tilePos) or costSoFar < node.costSoFar:#only add if it's not alredy there
+				openList.append(node)
 
 
 
 
-    print "No path :("
-    return None
-
+	print "No path :("
+	return None
 print "Starting!"
 HOST = '127.0.0.1'
 PORT = 1337
