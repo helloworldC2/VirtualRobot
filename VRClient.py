1113b33ba365bdff5ec8fd1817ef56e1e6c7b051
JoelBradley <joelbradley14@gmail.com>
2015-01-07 02:31:39 +0000

    Added client for VR

---
+++ VRClient.py
@@ -0,0 +1,15 @@
+import socket
+
+UDP_IP = "10.27.15.13" #ip of phone, use ifconfog in terminal
+UDP_PORT = 1337
+
+print "UDP target IP:", UDP_IP
+print "UDP target port:", UDP_PORT
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+
+def move(x,y):
+    data = str(x)+","+str(y)
+    print data
+    sock.sendto(data, (UDP_IP, UDP_PORT))
