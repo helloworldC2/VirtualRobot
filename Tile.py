e440c900e5c2742797d973d2c7db5b32c80356c8
Adma20 <adam.cook13@hotmail.com>
2015-01-12 16:07:08 +0000

    Update Tile.py
    I have added the new id system to allow us to use more tiles.

--- Tile.py
+++ Tile.py
@@ -6,8 +6,9 @@ class Tile(object):
 
 	
 
-	def __init__(self,iD,imagePath):
+	def __init__(self,iD,char,imagePath):
 		self.id = iD
+		self.char =char
 		self.img = pygame.image.load(imagePath)
 		self.img = pygame.transform.scale(self.img, (32, 32))
 		self.isSolid = False
@@ -35,16 +36,30 @@ class Tile(object):
 	def getId(self):
 		return self.id
 
-void = Tile(0,"tiles/void.png")
-grass = Tile(1,"tiles/grass.png")
-water = Tile(2,"tiles/water.png")
+void = Tile(0,"v","tiles/void.png")
+grass = Tile(1,"g","tiles/grass.png")
+water = Tile(2,"w","tiles/water.png")
 water.setSpeed(3)
-wall = Tile(3,"tiles/wall.png")
+wall = Tile(3,"a","tiles/wall.png")
 wall.setSolid(True)
-sand = Tile(4,"tiles/sand.png")
+sand = Tile(4,"s","tiles/sand.png")
 sand.setSpeed(2)
-redlight = Tile(5,"tiles/redlight.png")
+redlight = Tile(5,"r","tiles/redlight.png")
 redlight.setSolid(True)
-greenlight = Tile(6,"tiles/greenlight.png")
-start1 = Tile(7,"tiles/start.png")
-start2 = Tile(8,"tiles/start.png")
+greenlight = Tile(6,"l","tiles/greenlight.png")
+start1 = Tile(7,"S","tiles/start.png")
+start2 = Tile(8,"R","tiles/start.png")
+greenlight = Tile(9,"m","tiles/greenlight.png")
+Treasure0 = Tile(10,"z","tiles/start.png")
+Treasure2 = Tile(11,"x","tiles/start.png")
+Treasure3 = Tile(11,"y","tiles/start.png")
+Treasure4 = Tile(12,"p","tiles/start.png")
+
+
+def getID(char):
+	for t in tiles:
+		if t.char == char:
+			return t.id
+        return 0 
+
+
