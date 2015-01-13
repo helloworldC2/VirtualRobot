f16f748fe856d3a797175172a646dab2e81a967c
Joel Bradley <joelbradley14@gmail.com>
2015-01-13 18:35:02 +0000

    Added EmailClient
    Allows email to be sent to user to allow them to get more info on
    treasure they find

---
+++ EmailClient.py
@@ -0,0 +1,20 @@
+import requests
+
+
+"""Sends an email to the user to give them more info
+about a landmark/treasure they just found
+@Params:
+        email(string): Email address of user
+        landmark(string): name of landmark/treasure
+        landmarkImage(string): url to image of landmark/item
+        landmakerURL(string): link to page about landmark
+"""
+def sendEmail(email, landmark,landmarkImage,landmarkURL):
+    payload = {'to': email, 'link': landmarkURL,'image':landmarkImage, 'landmark':landmark}
+    r = requests.post("http://jrbradley.co.uk:8002/", data=payload)
+
+#an example email
+# sendEmail("aarondais12@gmail.com","Cabbages",
+# "http://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Cabbage_and_cross_section_on_white.jpg/300px-Cabbage_and_cross_section_on_white.jpg",
+# "http://en.wikipedia.org/wiki/Cabbage")
+
