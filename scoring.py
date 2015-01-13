68f0a310f7ec479afc53d29c73165ba0fa43f8df
Mythirion <aarondais12@gmail.com>
2015-01-13 14:28:15 +0000

    Create scoring.py

---
+++ scoring.py
@@ -0,0 +1,10 @@
+import LeaderboardClient
+
+score = 0
+
+if landmark == found:
+    score += 1
+
+postScore(name, player)
+
+####this is a work in progress, it is not currently working and i will change it when i need to, it's just a reference to me###
