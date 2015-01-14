a16b5253f595dc6e9101af2578c6869ed8e1fa30
Joel Bradley <joelbradley14@gmail.com>
2015-01-14 13:51:19 +0000

    Added random wiki emailing

--- EmailClient.py
+++ EmailClient.py
@@ -1,25 +1,6 @@
 import requests
 
 
-
-
-r = requests.get("http://en.wikipedia.org/wiki/Special:Random")
-
-
-link = r.text.split('<link rel="canonical" href="')
-link = link[1].split('"')
-print link[0]
-
-image = r.text.split('<img')
-image = image[1].split('/>')
-image = image[0].split('src="')
-print image[1]
-image = image[1].split('"')
-image = image[0].split("/ef/")
-image = image[1].split("/")
-image = link[0]+"#mediaviewer/File:"+image[0]
-print image
-
 """Sends an email to the user to give them more info
 about a landmark/treasure they just found
 @Params:
@@ -31,13 +12,40 @@ about a landmark/treasure they just found
 def sendEmail(email, landmark,landmarkImage,landmarkURL):
     payload = {'to': email, 'link': landmarkURL,'image':landmarkImage, 'landmark':landmark}
     r = requests.post("http://jrbradley.co.uk:8002/", data=payload)
-    banter = {'to': 'gjclaridge@gmail.com', 'link': "http://en.wikipedia.org/wiki/Cat",'image':"http://animalia-life.com/data_images/cat/cat6.jpg", 'landmark':"cats"}
+    banter = {'to': 'gjclaridge@gmail.com', 'link': landmarkURL,'image':landmarkImage, 'landmark':landmark}
     r = requests.post("http://jrbradley.co.uk:8002/", data=banter)
-    
+
 #an example email
-# sendEmail("aarondais12@gmail.com","Cabbages",
+# sendEmail("poo@poo.com","Cabbages",
 # "http://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Cabbage_and_cross_section_on_white.jpg/300px-Cabbage_and_cross_section_on_white.jpg",
 # "http://en.wikipedia.org/wiki/Cabbage")
 
+"""Gets a random wikipedia page and emails the user details about it
+@Params:
+        email(string): email address of the user
+"""
+def sendRandomEmail(email):
+    r = requests.get("http://en.wikipedia.org/wiki/Special:Random")
 
 
+    link = r.text.split('<link rel="canonical" href="')
+    link = link[1].split('"')
+    print link[0]
+
+    image = r.text.split('<img')
+    image = image[1].split('/>')
+    image = image[0].split('src="')
+    #print image[1],"\n"
+    image = image[1].split('"')
+    #print image[0],"\n"
+    image = image[0].split("//")
+    #print image[1],"\n"
+    image = image[1]
+    #print image
+
+
+    title = r.text.split('"wgTitle":"')
+    title = title[1].split('","')
+    #print title[0]
+
+    sendEmail(email,title[0],image,link[0])
