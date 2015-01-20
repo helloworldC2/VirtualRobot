1913d02cfeba250421685f49a44ba04be0185299
Mythirion <aarondais12@gmail.com>
2015-01-20 15:14:39 +0000

    Update scoring.py
    should work maybe

--- scoring.py
+++ scoring.py
@@ -2,9 +2,9 @@ import LeaderboardClient
 
 score = 0
 
-if landmark == found:
+def implementScore()
     score += 1
 
 postScore(name, player)
 
-####this is a work in progress, it is not currently working and i will change it when i need to, it's just a reference to me###
+
