215cea6d0b90bdf53028846bd385b5e875552546
Adma20 <adam.cook13@hotmail.com>
2015-01-21 12:46:14 +0000

    Update GuiHUD.py

--- GuiHUD.py
+++ GuiHUD.py
@@ -6,8 +6,8 @@ class GuiHUD(object):
     def __init___(self):
         pass
 
-    def render(self,screen,font):
-        text = font.render("Score: "+str(scoring.score), True, (0,0,0))
+    def render(self,screen,level,font):
+        text = font.render("Score: "+str(level.player.score.score), True, (0,0,0))
    	textpos = text.get_rect(center=(60,60))
         screen.blit(text, textpos)
 
