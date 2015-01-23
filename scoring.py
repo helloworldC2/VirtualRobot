d470d6ba490834f77b6ba86f1354af85c9f63385
Mythirion <aarondais12@gmail.com>
2015-01-23 13:23:09 +0000

    Update scoring.py
    comments, let me know if you need more

--- scoring.py
+++ scoring.py
@@ -2,11 +2,16 @@ import LeaderboardClient
 
 
 class Score(object):
+    """initialises the class
+    sets score variable to 0"""
     def __init__(self):
         self.score =  0
 
+    """uses incrementScore from LeaderboardClient
+    increases score by 25 points"""
     def incrementScore(self):
         self.score = self.score + 25
-
+    
+    """happyDucks is a function which posts the scores to the leaderboard client"""
     def happyDucks(self,name):
         LeaderboardClient.postScore(name, self.score)
