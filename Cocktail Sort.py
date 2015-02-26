cb1c742c7b2bfa291ef7bcf5851980bfdeccc517
Adma20 <adam.cook13@hotmail.com>
2015-02-26 21:57:33 +0000

    Cocktail search
    Conflicts:
    	tiles/Burnt chest.png
    	tiles/Burnt_chest.png
    	tiles/Crystal chest.png
    	tiles/Crystal_chest.png
    	tiles/Dark chest.png
    	tiles/Dark_chest.png
    	tiles/Glass chest.png
    	tiles/Glass_chest.png
    	tiles/Gold chest.png
    	tiles/Gold_chest.png
    	tiles/Grown over chest.png
    	tiles/Grown_over_chest.png
    	tiles/Normal chest.png
    	tiles/Normal_chest.png
    	tiles/broken  chest.png
    	tiles/broken _chest.png
    	tiles/brokenChest.png
    	tiles/burntChest.png
    	tiles/crystalChest.png
    	tiles/darkChest.png
    	tiles/glassChest.png
    	tiles/goldChest.png
    	tiles/grownOverChest.png
    	tiles/normalChest.png

--- Cocktail Sort.py
+++ Cocktail Sort.py
@@ -6,10 +6,10 @@ def createJobs(alist,worker):
         jobs.append(Jobs.JobPlaceTreasure(i,treasure=t))
         
     exchanges = True
-    passnum = len(alist)-1
+    passnum = len(alist)-2
     while passnum > 0 and exchanges:
        exchanges = False
-       for i in range(passnum):
+       for i in range(len(jobs)-2):
            if alist[i]>alist[i+1]:
                exchanges = True
                temp = alist[i]
@@ -25,8 +25,26 @@ def createJobs(alist,worker):
                jobs.append(Jobs.JobGoTo(i+1))
                jobs.append(Jobs.JobSwapHandWithContainer())
                jobs.append(Jobs.JobPlaceTreasure(i+1))
+               exchanges = True
+       if exchanges == False:break
+       for i in range(len(jobs)-2,0,-1):
+           if alist[i]>alist[i+1]:
+               jobs.append(Jobs.JobGoTo(i))
+               jobs.append(Jobs.JobPickUpTreasure(i))
+               jobs.append(Jobs.JobStoreTreasure())
+               alist[i] = alist[i+1]
+               jobs.append(Jobs.JobGoTo(i+1))
+               jobs.append(Jobs.JobPickUpTreasure(i+1))
+               jobs.append(Jobs.JobGoTo(i))
+               jobs.append(Jobs.JobPlaceTreasure(i))
+               alist[i+1] = temp
+               jobs.append(Jobs.JobGoTo(i+1))
+               jobs.append(Jobs.JobSwapHandWithContainer())
+               jobs.append(Jobs.JobPlaceTreasure(i+1))
+               exchanges == True
 
-               
+                               
        passnum = passnum-1
     jobs.append(Jobs.JobIdle())
     return jobs
+
