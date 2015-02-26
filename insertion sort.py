bf6b22a8f20389d5e30d7e1d67dbe8bc69803d08
Riki Dhesi <rikidhesi@hotmail.co.uk>
2015-02-26 19:23:41 +0000

    Create new file
    not the final finished sorting algorithm, uploaded it cause I didnt have a copy at home.

---
+++ insertion sort.py
@@ -0,0 +1,15 @@
+def insertionSort(alist):
+   for index in range(1,len(alist)):
+
+     currentvalue = alist[index]
+     position = index
+
+     while position>0 and alist[position-1]>currentvalue:
+         alist[position]=alist[position-1]
+         position = position-1
+
+     alist[position]=currentvalue
+
+alist = [54,26,93,17,77,31,44,55,201,5,8,96,1025,4248,350,48336,12542]
+insertionSort(alist)
+print(alist)
