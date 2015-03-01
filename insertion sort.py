65127ca4f59c272c7ecbb93aee64154e329a99a6
Riki Dhesi <rikidhesi@hotmail.co.uk>
2015-03-01 16:43:53 +0000

    Update insertion sort.py
    insertion sorting algorithm

--- insertion sort.py
+++ insertion sort.py
@@ -1,15 +1,29 @@
-def insertionSort(alist):
-   for index in range(1,len(alist)):
+import Jobs
+def createJobs(alist,worker):
+    jobs = []
+    for index in range(1,len(alist)):
+       jobs.append(Jobs.JobGoTo(i))
+       jobs.append(Jobs.JobPlaceTreasure(i,treasure=t))
 
-     currentvalue = alist[index]
-     position = index
+    currentvalue = alist[index]
+    position = index
 
-     while position>0 and alist[position-1]>currentvalue:
-         alist[position]=alist[position-1]
-         position = position-1
+    while position > 0 and alist[postion-1]>currentvalue:
+       jobs.append(Jobs.JobGoTo(i))
+       jobs.append(Jobs.JobPickUpTreasure(i))
+       jobs.append(Jobs.JobStoreTreasure())
+       alist[position]=alist[position-1]
+       jobs.append(Jobs.JobGoTo(i+1))
+       jobs.append(Jobs.JobPickUpTreasure(i+1))
+       jobs.append(Jobs.JobGoTo(i))
+       jobs.append(Jobs.JobPlaceTreasure(i))
+       alist[postion] = currentvalue
+       jobs.append(Jobs.JobGoTo(i+1))
+       jobs.append(Jobs.JobSwapHandWithContainer())
+       jobs.append(Jobs.JobPlaceTreasure(i+1))
 
-     alist[position]=currentvalue
+       position = position-1
+       jobs.append(Jobs.JobIdle())
+       insertionSort (alist)
+    return Jobs
 
-alist = [54,26,93,17,77,31,44,55,201,5,8,96,1025,4248,350,48336,12542]
-insertionSort(alist)
-print(alist)
