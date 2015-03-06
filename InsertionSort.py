13f34db6b5fe0b1b6c6b807c1db0803c03ede470
Riki Dhesi <rikidhesi@hotmail.co.uk>
2015-03-06 16:51:02 +0000

    Update InsertionSort.py

--- InsertionSort.py
+++ InsertionSort.py
@@ -1,29 +1,28 @@
 import Jobs
-def createJobs(alist,worker):
+def insertionSort(alist):
     jobs = []
     for index in range(1,len(alist)):
-       jobs.append(Jobs.JobGoTo(i))
-       jobs.append(Jobs.JobPlaceTreasure(i,treasure=t))
+       jobs.append(Jobs.JobGoTo(index))
+       jobs.append(Jobs.JobPlaceTreasure(index))
 
     currentvalue = alist[index]
     position = index
 
-    while position > 0 and alist[postion-1]>currentvalue:
-       jobs.append(Jobs.JobGoTo(i))
-       jobs.append(Jobs.JobPickUpTreasure(i))
+    while position > 0 and alist[position-1]>currentvalue:
+       jobs.append(Jobs.JobGoTo(index))
+       jobs.append(Jobs.JobPickUpTreasure(index))
        jobs.append(Jobs.JobStoreTreasure())
        alist[position]=alist[position-1]
-       jobs.append(Jobs.JobGoTo(i+1))
-       jobs.append(Jobs.JobPickUpTreasure(i+1))
-       jobs.append(Jobs.JobGoTo(i))
-       jobs.append(Jobs.JobPlaceTreasure(i))
-       alist[postion] = currentvalue
-       jobs.append(Jobs.JobGoTo(i+1))
+       jobs.append(Jobs.JobGoTo(index+1))
+       jobs.append(Jobs.JobPickUpTreasure(index+1))
+       jobs.append(Jobs.JobGoTo(index))
+       jobs.append(Jobs.JobPlaceTreasure(index))
+       alist[position] = currentvalue
+       jobs.append(Jobs.JobGoTo(index+1))
        jobs.append(Jobs.JobSwapHandWithContainer())
-       jobs.append(Jobs.JobPlaceTreasure(i+1))
+       jobs.append(Jobs.JobPlaceTreasure(index+1))
 
        position = position-1
        jobs.append(Jobs.JobIdle())
-       insertionSort (alist)
+       
     return Jobs
-
